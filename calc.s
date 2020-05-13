bf6e23527be7ce200a4cb125b0722827eefd5b45
sagivbgu <sagivsc@post.bgu.ac.il>
2020-05-13 13:24:53 +0300

    Finished main

--- calc.s
+++ calc.s
@@ -1,13 +1,14 @@
 section .bss
     stack: resd 1 ; Address of the operand stack
 
+section .rodata
+    printNumberFormat: db "%d", 10, 0
+
 section .data
     debug: db 0
     stackSize: db 5 ; Operand stack size (default: 5. Min: 2, Max: 0xFF)
     itemsInStack: db 0 ; Current items in stack (start value: 0)
     
-; TODO: REMOVE THIS LINE    dashSign: equ 45 ; '-'
-
 section .text                    	
     align 16
     global main
@@ -45,30 +46,69 @@ main:
             jmp endParseArgument
 
         parseStackSize:
-            and edx, 0x0000FFFF ; Now the first 2 bytes (= last 2 chars of a string) of edx are zeros
-            
-            ;
-            ; TODO: Convert string to number
-            ;
+            pushad
+            push dx
+            call hexStringToByte
+            mov [stackSize], al
+            add esp, 2 ; "Remove" pushed dx from stack
+            popad
 
         endParseArgument:
             add ebx, 4
             loop parseArgument, ecx ; Decrement ecx and if ecx != 0, jump to the label
 
     ; Call the primary loop
-    callMyCalc:
-        push dword 0 ; so that [ebp-4] = operations performed (start value: 0. Max: 2^31)
-        call myCalc
+    callMyCalc: call myCalc
         
     ; Print number of operations performed
-    ; Assuming operations performed is still on the stack
-    push dword 0x00006425; "%d\0\0"
+    ; Assuming operations performed is in eax
+    push eax
+    push printNumberFormat
     call printf
 
     finishProgram:
         add esp, 8
-        mov eax, 0
+        mov eax, 0 ; Program exit code
         ret
 
 myCalc:
-    ; TODO
+    ; push dword 0 ; operations performed (start value: 0. Max: 2^31)
+    mov eax, 0 ; TODO: CHANGE THIS LINE TO THE REAL CODE
+    ret
+
+
+; Get a word representing 2 hexadecimal digits and return the value they represent.
+; Result stored in al.
+hexStringToByte:
+    mov ebp, esp
+
+    mov dx, [ebp+4]
+    push dx
+    call hexCharToValue
+    ; al contains the value of the first letter
+
+    shr dx, 8 ; So that dl = dh, dh = 0
+    cmp dl, 0 ; If it's a null byte, ignore it
+    jz returnStringValue ; al already contains the desired value
+
+    shl al, 4 ; Multiply value by 0x10
+    mov cl, al
+    push dx
+    call hexCharToValue
+    
+    add al, cl
+
+    returnStringValue:
+        mov esp, ebp
+        ret
+
+; Get a byte representing a hexadecimal digit and return the value it represents.
+; Result stored in al.
+hexCharToValue:
+    mov al, [esp+4]
+    sub al, 0x30
+    cmp al, 9 ; Check if it's a char between '0' and '9'
+    jle returnCharValue
+    ; Now we know it's a char between 'A' and 'F'
+    sub al, 0x7 ; Correct according to offset in ascii table
+    returnCharValue: ret
