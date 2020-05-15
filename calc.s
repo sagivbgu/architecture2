10850447051f43b9bdc880ce9d9075fe90388c25
zohar <zohar275@gmail.com>
2020-05-15 15:53:37 +0300

    Started myCalc (not working yet)

--- calc.s
+++ calc.s
@@ -1,26 +1,72 @@
 section .bss
     stack: resd 1 ; Address of the operand stack
+    buffer: resb 81 ; The input buffer - 81 bc \n is also saved 
 
 section .rodata
     printNumberFormat: db "%d", 10, 0
+    calcMsg: db "calc: ",0
+    lenCalc: equ$ -calcMsg
+
 
 section .data
     debug: db 0
     stackSize: db 5 ; Operand stack size (default: 5. Min: 2, Max: 0xFF)
     itemsInStack: db 0 ; Current items in stack (start value: 0)
-    
+
 section .text                    	
     align 16
     global main
     extern printf
-    extern fprintf
+    extern fprintf 
     extern fflush
-    extern malloc
-    extern calloc
-    extern free
-    extern gets
-    extern getchar
-    extern fgets
+    extern malloc 
+    extern calloc 
+    extern free 
+    extern gets 
+    extern getchar 
+    extern fgets 
+
+%macro quit 1 ;we get the stack so we can free it, and we get the numbers of operands so we can print it
+    freeStack 1
+%endmacro
+
+%macro print 2
+    mov ebx, 1
+    mov ecx, %1 ;string
+    mov edx, %2 ;how many bytes to print
+    mov eax, 4
+    int 0x80
+%endmacro
+
+%macro freeStack  ; we free the nodes from last to first - but its not really matter
+    mov ebx, stackSize
+    startLoop:
+        mov eax, [stack + 4*stackSize] ;gets last place in the stack equals to pop
+        freeNum eax ; sends the last address so we can free it
+        dec ebx
+        cmp ebx, 0
+        je endLoop
+        mov eax, 4
+        jump startLoop
+    endLoop:
+    pushad
+    push stack ;free the stack memory
+    call free
+    popad
+%endmacro
+
+%malloc freeNum 1 ; gets address
+;TODO 
+%endmacro
+
+%macro pushReturn
+    push edx
+    push ecx
+    push ebx
+    push esi
+    push edi
+%endmacro
+
 
 main:
     ; Process command line arguments (all optional): "-d" or stackSize
@@ -72,9 +118,49 @@ main:
         ret
 
 myCalc:
-    ; push dword 0 ; operations performed (start value: 0. Max: 2^31)
-    mov eax, 0 ; TODO: CHANGE THIS LINE TO THE REAL CODE
-    ret
+    pushad ;fix to return push
+    mov eax, 4 
+    push eax
+    mov eax, [stackSize] ; stack size - we need to check somewhere else if the itemsOnStack is valid
+    push eax   
+    call calloc        
+    mov stack, dword [eax] ; eax has the pointer to the start of the stack
+    popad
+    
+    calcLoop:
+        print calcMsg, lenCalc
+        ;we need to backup the registers
+        mov eax, 3 ; lines 82-86 reads the input to the buffer - eax has the number of bytes that have been recived - the input is valid no need to check
+        mov ebx, 0
+        mov ecx, buffer
+        mov edx, 81
+        int 0x80 
+        dec eax ; lenght of the char without the \n
+
+        cmp byte [buffer], 'q'
+        je endCalcLoop
+        cmp byte [buffer], '+'
+        je 
+        cmp byte [buffer], 'p'
+        je 
+        cmp byte [buffer], 'd'
+        je 
+        cmp byte [buffer], '&'
+        je 
+        cmp byte [buffer], '|'
+        je 
+        cmp byte [buffer], 'n'
+        je 
+        cmp byte [buffer], '*'
+        je 
+        ;its a number so we need to parse it
+        ;add to the stack
+        jump calcLoop
+
+    endCalcLoop:
+        quit stack
+        mov eax, [itemsInStack]
+        ret
 
 
 ; Get a word representing 2 hexadecimal digits and return the value they represent.
@@ -95,7 +181,7 @@ hexStringToByte:
     mov cl, al
     push dx
     call hexCharToValue
-    
+
     add al, cl
 
     returnStringValue:
